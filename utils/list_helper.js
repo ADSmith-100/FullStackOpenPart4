e6e59a0df669d925ccc881e213ea9258e739ac7f
Adam Smith <coloradoasmith@gmail.com>
2020-11-25 15:34:29 -0700

    totalLikes working, 4.4 complete

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 1) {
+    return blogs[0].likes;
+  } else if (blogs.length === 0) {
+    return 0;
+  } else {
+    const reducer = (sum, item) => {
+      return sum + item.likes;
+    };
+    return blogs.reduce(reducer, 0);
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
