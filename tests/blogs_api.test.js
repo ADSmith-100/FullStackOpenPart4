b33b51266dcf1d1f05c6d2032219b6a23a032a43
Adam Smith <coloradoasmith@gmail.com>
2020-12-02 18:33:09 -0700

    4.13 complete

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -82,6 +82,23 @@ test("if the title and url properties are missing, the backend responds 400 Bad 
   await api.post("/api/blogs").send(missingTitleUrlBlog).expect(400);
 });
 
+describe("deletion of a blog", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((r) => r.title);
+
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
