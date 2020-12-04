a53ad74de9d1c906fda595d3e04d32683c76ce57
Adam Smith <coloradoasmith@gmail.com>
2020-12-04 09:41:24 -0700

    4.17 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
@@ -21,14 +26,19 @@ blogsRouter.get("/:id", (request, response, next) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const blog = new Blog({
+    url: body.url,
     title: body.title,
     author: body.author,
-    url: body.url,
+    user: user._id,
     likes: body.likes || 0,
   });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.json(savedBlog);
 });
 
