47575dcad5d1133826e26bfa3f64b8bf5253e5d6
Adam Smith <coloradoasmith@gmail.com>
2020-12-04 13:58:35 -0700

    4.20 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -35,7 +35,7 @@ blogsRouter.get("/:id", (request, response, next) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: "token missing or invalid" });
   }
