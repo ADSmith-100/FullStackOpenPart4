70dadebf1f3049f9cafbf70b5c853b32a56a0e46
Adam Smith <coloradoasmith@gmail.com>
2020-12-03 20:10:43 -0700

    adds ability to create new user and a get all users route 4.15 complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
